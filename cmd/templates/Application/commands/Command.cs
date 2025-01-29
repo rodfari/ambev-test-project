d5e4a59b7dd71386ca9fc5a25a46e85732954d41
rodfari <rodrigo.code@outlook.com>
2025-01-29 20:08:06 -0300

    creact project removed

--- cmd/templates/Application/commands/Command.cs
+++
@@ -1,9 +0,0 @@
-using Core.Application.Reponses;
-using Core.Application.Reponses.$"FEATURE_NAME";
-using MediatR;
-
-namespace Core.Application.Feature.Posts.Commands."$FEATURE_NAME";
-public class "$FEATURE_NAME"Command: IRequest<TResponse<"$FEATURE_NAME"Response>>
-{
-
-}
--- cmd/templates/Application/commands/Command.sh
+++
@@ -1,9 +0,0 @@
-using Core.Application.Reponses;
-using Core.Application.Reponses.FEATURE_NAME_PLACEHOLDER;
-using MediatR;
-
-namespace Core.Application.Feature.Posts.Commands.FEATURE_NAME_PLACEHOLDER;
-public class FEATURE_NAME_PLACEHOLDERCommand: IRequest<TResponse<FEATURE_NAME_PLACEHOLDERResponse>>
-{
-
-}
